78de740ea098c1767455f4def686d4e0f0b380bf
Jared <jaredcmurphy@gmail.com>
2016-03-03 11:27:44 -0500

    adds highcharts for states and animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,117 @@
 //= require highcharts/highcharts-more
 //= require highcharts/highstock
 
-$( document ).ready(function(){
- console.log('application script loaded');
+$( document ).ready(function() {
+    console.log( "js loaded" );
+    $('.earth').on("click", function() {
+        $(this).addClass('magictime holeOut');
+
+       $('.earth').fadeOut(800);
+        $('p').css("color: black");
+
+        setTimeout(function(){
+        $('body').css("background-image", "");
+        $('body').css("background-color", "white");
+
+          setTimeout(function() {
+            $('.heading').show().addClass('magictime tinRightIn');
+          }, 1000)
+
+          setTimeout(function() {
+            $('.sub_heading').show().addClass('magictime tinRightIn');
+          }, 2000)
+
+          setTimeout(function() {
+            $('.sub_sub_heading').show().addClass('magictime tinRightIn');
+          }, 3000)
+          setTimeout(function(){
+
+
+            $('.state_select').fadeIn();
+          }, 4500)
+
+        }, 1000);
+  }); // ends earth click function
+  $state = $('.state')
+  $('.state_select').on("click", function() { stateNumber = $(this).attr('id')
+  console.log(stateNumber);
+  $('.state_select').css("fill"," blue");
+  $(this).css("fill", "red");
+  var $values = [];
+      $.ajax({
+        method: 'GET',
+        url: "http://www.ncdc.noaa.gov/cag/time-series/us/" + stateNumber + "/00/tavg/ytd/12/1895-2016.json?base_prd=true&firstbaseyear=1901&lastbaseyear=2000",
+        success: function(data) {
+        data = JSON.parse(data)
+        mo_data = data.data
+        //for (var prop in mo_data) { $values.push(parseFloat(mo_data[prop]["anomaly"]))}
+        stateName = data.description.title.split(",")[0]
+       // console.log(stateNumber + stateName)
+        //console.log($values)
+        Object.keys(data.data).sort().forEach(function(i){
+        //  console.log(i)
+
+          for (var prop in mo_data) {
+              if (prop === i) {
+                 $values.push(parseFloat(mo_data[prop]["anomaly"]))
+                 console.log("year: " + i + " value: " + mo_data[prop]["anomaly"]);
+              }
+          }
+        })
+
+        firstYear = Object.keys(data.data).sort()[0]
+        //console.log(stateName + "first year: " + firstYear.slice(0,4))
+          var $years = [];
+          for (var i = firstYear.slice(0,4); i < 2016; i++){
+            $years.push(i);
+            }
+          console.log(stateName + " " + $years)
+$(function () {
+    $('#container').show();
+    $('#container').highcharts({
+        title: {
+            text: 'Temperature Anomolies from the 20th Century Average',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: ncdc.noaa.com',
+            x: -20
+        },
+        xAxis: {
+            categories: $years,
+            labels: {step: 5}
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: stateName,
+            data: $values,
+            color: 'red',
+            negativeColor: 'blue'
+        }]
+    });
 });
+}
+
+
+});
+});
+});
+

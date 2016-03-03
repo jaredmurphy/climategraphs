46a93f8e7f5aa4596a800ead33a388684a2eb8dc
Jared <jaredcmurphy@gmail.com>
2016-03-03 13:23:53 -0500

    fixes routing for /

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :data_points
   resources :continents
   resources :data_points
+  get '/' => 'data_points#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
